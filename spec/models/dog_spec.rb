f1bfc164006d2f48d30fb0e8c89d6ff6dfd4f291
RodCato <code@eliment.org>
2023-06-06 17:46:41 -0500

    Added tests for breed length and age range

--- spec/models/dog_spec.rb
+++ spec/models/dog_spec.rb
@@ -65,4 +65,44 @@ RSpec.describe Dog, type: :model do
     )
     expect(dog.errors[:image]).to_not be_empty
   end
+  
+  it 'cannot add a dog with a name less than 4 characters' do
+    dog = Dog.create(
+      {
+        name:'Pon',
+        age:8,
+        breed: 'Dalmation',
+        sex:'M',
+        image: 'https://static.wikia.nocookie.net/disney/images/3/3c/Profile_-_Pongo.png/revision/latest?cb=20200504214003'
+      }
+    )
+    expect(dog.errors[:name]).to_not be_empty
+  end
+
+  it 'cannot add a dog with a breed less than 4 characters' do
+    dog = Dog.create(
+      {
+        name:'Pongo',
+        age:8,
+        breed: 'Dal',
+        sex:'M',
+        image: 'https://static.wikia.nocookie.net/disney/images/3/3c/Profile_-_Pongo.png/revision/latest?cb=20200504214003'
+      }
+    )
+    expect(dog.errors[:breed]).to_not be_empty
+  end
+
+  it 'cannot add a dog with an age younger than 1 or older than 7' do
+    dog = Dog.create(
+      {
+        name:'Pongo',
+        age:8,
+        breed: 'Dalmation',
+        sex:'M',
+        image: 'https://static.wikia.nocookie.net/disney/images/3/3c/Profile_-_Pongo.png/revision/latest?cb=20200504214003'
+      }
+    )
+    expect(dog.errors[:age]).to_not be_empty
+  end
+
 end
